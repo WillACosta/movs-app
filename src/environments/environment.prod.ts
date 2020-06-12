f84bc55d993f06a27f6a774cb2be1d1f43f58a3d
William <williamamaral521@gmail.com>
2020-06-12 17:53:06 -0300

    Implementações finalizadas

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA9Bors68yAxTIKoKBScH5kyO_H0suISwE',
+    authDomain: 'erp-app-213b8.firebaseapp.com',
+    databaseURL: 'https://erp-app-213b8.firebaseio.com',
+    projectId: 'erp-app-213b8',
+    storageBucket: 'erp-app-213b8.appspot.com',
+    messagingSenderId: '387777440545',
+    appId: '1:387777440545:web:b6883fd9c9e9dff6b86c60',
+    measurementId: 'G-ZSNHV3QMZ4',
+  },
 };
